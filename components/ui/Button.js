89f658e0bd88bb32e6f6a8d263d8b2930a808e57
fevin1414 <fevinbiju1414@gmail.com>
2023-09-07 14:38:21 -0500

    added filter

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -3,11 +3,14 @@ import Link from "next/link"; // Corrected import statement
 import classes from "./button.module.css";
 
 const ButtonExplore = (props) => {
+  if(props.link){
   return (
     <Link href={props.link} passHref>
       <a className={classes.btn}>{props.children}</a>
     </Link>
   );
+  }
+  return <button className={classes.btn} onClick={props.onClick}>{props.children}</button>
 };
 
 export default ButtonExplore;
